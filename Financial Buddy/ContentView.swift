ffa3f7d12319ad25c4b59179e72bed4d6afec689
Robert San <robertsandru96@gmail.com>
2020-05-07 19:52:56 +0300

    Initial Commit

---
+++ Financial Buddy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Financial Buddy
+//
+//  Created by Robert Sandru on 5/7/20.
+//  Copyright © 2020 codecontrive. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
